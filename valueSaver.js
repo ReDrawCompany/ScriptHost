3690949ddb77ad9dfae5ad07896115118ee04f7d
ReDrawCompany <49760297+ReDrawCompany@users.noreply.github.com>
2019-04-18 11:51:07 -0400

    format

--- valueSaver.js
+++ valueSaver.js
@@ -1,26 +1,26 @@
-(function(){
-    localStorage.savedData = JSON.stringify((typeof localStorage.savedData == "string") ? JSON.parse(localStorage.savedData) : {});
-    let boxes = document.querySelectorAll(".saveValue");
-    let data = JSON.parse(localStorage.savedData);
-    if (typeof data[location.pathname] === "undefined") {
-        data[location.pathname] = {value: [], textContent: [], checked: []};
+( function() {
+    localStorage.savedData = JSON.stringify(( typeof localStorage.savedData == "string" ) ? JSON.parse( localStorage.savedData ) : {});
+    let boxes = document.querySelectorAll( ".saveValue" );
+    let data = JSON.parse( localStorage.savedData );
+    if( typeof data[ location.pathname ] === "undefined" ) {
+        data[ location.pathname ] = { value: [], textContent: [], checked: [] };
     }
     let dataOrig = data;
-    data = data[location.pathname];
-    console.log(boxes);
+    data = data[ location.pathname ];
+    console.log( boxes );
 
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].value = data.value[i];
-        boxes[i].textContent = data.textContent[i];
-        boxes[i].checked = data.checked[i];
+    for( let i = 0;i < boxes.length;i++ ) {
+        boxes[ i ].value = data.value[ i ];
+        boxes[ i ].textContent = data.textContent[ i ];
+        boxes[ i ].checked = data.checked[ i ];
     }
 
     setInterval(() => {
-        for (let i = 0; i < boxes.length; i++) {
-            data.value[i] = boxes[i].value;
-            data.textContent[i] = boxes[i].textContent;
-            data.checked[i] = boxes[i].checked;
+        for( let i = 0;i < boxes.length;i++ ) {
+            data.value[ i ] = boxes[ i ].value;
+            data.textContent[ i ] = boxes[ i ].textContent;
+            data.checked[ i ] = boxes[ i ].checked;
         }
-        localStorage.savedData = JSON.stringify((typeof localStorage.savedData == "string") ? dataOrig : {});
-    }, 2000);
+        localStorage.savedData = JSON.stringify(( typeof localStorage.savedData == "string" ) ? dataOrig : {});
+    }, 2000 );
 })();
